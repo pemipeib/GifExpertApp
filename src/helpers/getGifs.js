4143a84b4bb697b42ce563637127ae6a968665d7
pemipeib <p3d80p3832>
2020-11-16 22:23:32 +0100

    Fin del proyecto

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+
+export const getGifs = async ( category ) => {
+    const apiKey = 'PUUrcz0gdKvJ2Kk80H0V25xPYN4sKePt';
+    const limit = 10;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=${limit}&api_key=${apiKey}`;
+    const respuesta = await fetch(url);
+    const {data} = await respuesta.json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    return gifs;
+}
